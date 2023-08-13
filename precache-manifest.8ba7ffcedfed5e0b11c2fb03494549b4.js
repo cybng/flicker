9e35d8123519f6fec085675302d4be60f61152f3
it <macu@macus-MacBook-Air.local>
2023-08-13 17:33:35 +0530

    ecomm

---
+++ precache-manifest.8ba7ffcedfed5e0b11c2fb03494549b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19b071b0cbac28e332310aec22d1544a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2154ffbf644ebc695f9",
+    "url": "/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "f47681a3924e6baaa238",
+    "url": "/static/css/main.314983d0.chunk.css"
+  },
+  {
+    "revision": "f2154ffbf644ebc695f9",
+    "url": "/static/js/2.4a2bda21.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4a2bda21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f47681a3924e6baaa238",
+    "url": "/static/js/main.fe48d12b.chunk.js"
+  },
+  {
+    "revision": "4e0e64804998ed220b77",
+    "url": "/static/js/runtime-main.4b521ca0.js"
+  }
+]);
